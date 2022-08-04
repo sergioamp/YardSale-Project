cf21a7d7c8c24eb4345a0e6a376549999decaba8
Sergio Medina Papagayo <sergio.m.papagayo@gmail.com>
2022-08-04 10:42:54 -0500

    Se adiciona el menu desplegable

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+let URLactual = window.location;
+alert(URLactual);
+
+if(URLactual == 'http://127.0.0.1:5500/HTML/index.html') {
+    const menuIcon = document.querySelector('.navbar__menu-icon');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    menuIcon.addEventListener('click', toggleMobileMenu);
+
+    function toggleMobileMenu(){
+        mobileMenu.classList.toggle('inactive');
+    }
+
+}
+if(URLactual == 'http://127.0.0.1:5500/HTML/login.html') {
+
+}
