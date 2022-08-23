5244489e23d1d1e9cfe4fe149754a35d6b436d69
Sergio Medina Papagayo <sergio.m.papagayo@gmail.com>
2022-08-22 22:04:04 -0500

    my account and edit my account have been added

--- JS/main.js
+++ JS/main.js
@@ -5,6 +5,9 @@ const menuIcon = document.querySelector('.menu-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
 let isUserLoggedIn = false;
 const desktopMenu = document.querySelector('.desktop-menu');
+const navbarRightGroup = document.querySelector('.navbar__right-group');
+const myAccount = document.querySelector('.my-account');
+const editMyAccount = document.querySelector('.edit-my-account');
 
 function renderProducts(products) {
   for (product of products) {
@@ -169,6 +172,23 @@ function toggleLogin() {
   }
 }
 
+function closeOpenwindows() {
+  const isMobileMenuOpen = !mobileMenu.classList.contains('close');
+  const isProductDetailOpen = !productDetail.classList.contains('close');
+  const isMyAccountOpen = !myAccount.classList.contains('close');
+  const isDesktopMenuOpen = !desktopMenu.classList.contains('close');
+
+  if (isMobileMenuOpen || isDesktopMenuOpen){
+    mobileMenu.classList.add('close');
+    desktopMenu.classList.add('close');
+    menuIcon.classList.remove('open');
+  }
+  if (isProductDetailOpen || isMyAccountOpen){
+    productDetail.classList.add('close');
+    myAccount.classList.add('close');
+  }
+}
+
 function renderProductDetail(productRef) {
   const productImg = document.querySelector('#product-detail__image');
   productImg.setAttribute('src', productList[productRef - 1].image);
@@ -186,6 +206,7 @@ function renderProductDetail(productRef) {
 
 function openProductDetail() {
   const element = event.target.getAttribute("id");
+  closeOpenwindows();
   renderProductDetail(element);
   productDetail.classList.remove('close');
 }
@@ -200,18 +221,43 @@ function toggleMobileMenu() {
     menuIcon.classList.add('open');
     mobileMenu.classList.remove('close');
   } else {
+    closeOpenwindows();
     menuIcon.classList.remove('open');
     mobileMenu.classList.add('close');
   }
 }
 
 function toggleDesktopMenu() {
-  const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+  const isDesktopMenuOpen = !desktopMenu.classList.contains('close');
   if (isDesktopMenuOpen){
-    desktopMenu.classList.add('inactive');
+    desktopMenu.classList.add('close');
   } else {
-    desktopMenu.classList.remove('inactive');
+    closeOpenwindows();
+    desktopMenu.classList.remove('close');
   }
+}
+
+function openMyAccount() {
+  closeOpenwindows();
+  menuIcon.classList.add('invisible');
+  navbarRightGroup.classList.add('invisible');
+  myAccount.classList.remove('close');
+}
+
+function openEditWindow() {
+  myAccount.classList.add('close');
+  editMyAccount.classList.remove('close');
+}
+
+function closeEditWindow() {
+  myAccount.classList.remove('close');
+  editMyAccount.classList.add('close');
+}
+
+function goToHome() {
+  menuIcon.classList.remove('invisible');
+  navbarRightGroup.classList.remove('invisible');
+  closeOpenwindows();
 }
 
 function signOut() {
