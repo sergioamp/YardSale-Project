ea8c38460af011c591f85850c3aa6e9293623f0d
Sergio Medina Papagayo <sergio.m.papagayo@gmail.com>
2022-11-17 00:45:20 -0500

    Optimización de los selectores. Se crean nuevas clases para separar CSS de JS

--- JS/main.js
+++ JS/main.js
@@ -1,38 +1,38 @@
 let isInHome = true;
 let isUserLoggedIn = false;
 
-const logo = document.querySelector('.yardsale-logo');
-const navbarNav = document.querySelector('.navbar-nav');
-const categories = document.querySelector('.navbar-nav--left');
-const loginLink = document.querySelector('.login-link');
-const signupLink = document.querySelector('.signup-link');
-const userNameDesktop = document.querySelector('.username-desktop');
-const userNameMobile = document.querySelector('.username-mobile');
-const icons = document.querySelector('.icons');
-const hamburguerIcon = document.querySelector('.hambuguer-icon');
-const cartIcon = document.querySelector('.cart-icon');
-const cartIconSvg = document.querySelector('.cart-icon__svg');
-const loginSection = document.querySelector('.login-section');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const signupSection = document.querySelector('.signup-section');
-const newUserName = document.getElementById('new-user-name');
-const newUserEmail = document.getElementById('new-user-email');
-const newUserPassword = document.getElementById('new-user-password1');
-const confirmNewUserPassword = document.getElementById('new-user-password2');
-const recoveryPasswordSection = document.querySelector('.recover-password-section');
-const sendEmail = document.getElementById('send-email');
-const emailConfirmationSection = document.querySelector('.email-confirmation-section');
-const desktopMenu = document.querySelector('.desktop-menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-const mobileMenuCategories = document.querySelector('.mobile-menu__categories');
-const mobileMenuUser = document.querySelector('.mobile-menu__user');
-const mobileMenuLogin = document.querySelector('.mobile-menu__login');
-const myAccountSection = document.querySelector('.my-account-section');
-const editAccountSection = document.querySelector('.edit-account-section');
-const newEmail = document.getElementById('new-email');
-const newPassword = document.getElementById('new-password1');
-const confirmNewPassword = document.getElementById('new-password2');
+const $logo = document.querySelector('.logo--js');
+const $navbarNav = document.querySelector('.navbar-nav--js');
+const $categoriesDesktop = document.querySelector('.categories-desktop--js');
+const $loginLink = document.querySelector('.login-link--js');
+const $signupLink = document.querySelector('.signup-link--js');
+const $userNameDesktop = document.querySelector('.username-desktop--js');
+const $userNameMobile = document.querySelector('.username-mobile--js');
+const $icons = document.querySelector('.icons--js');
+const $hamburguerIcon = document.querySelector('.hambuguer-icon--js');
+const $cartIcon = document.querySelector('.cart-icon--js');
+const $cartIconSvg = document.querySelector('.cart-icon__svg--js');
+const $loginSection = document.querySelector('.login-section--js');
+const $email = document.getElementById('email');
+const $password = document.getElementById('password');
+const $signupSection = document.querySelector('.signup-section--js');
+const $newUserName = document.getElementById('new-user-name');
+const $newUserEmail = document.getElementById('new-user-email');
+const $newUserPassword = document.getElementById('new-user-password1');
+const $confirmNewUserPassword = document.getElementById('new-user-password2');
+const $recoveryPasswordSection = document.querySelector('.recover-password-section--js');
+const $sendEmail = document.getElementById('send-email');
+const $emailConfirmationSection = document.querySelector('.email-confirmation-section--js');
+const $desktopMenu = document.querySelector('.desktop-menu--js');
+const $mobileMenu = document.querySelector('.mobile-menu--js');
+const $mobileMenuCategories = document.querySelector('.mobile-menu__categories--js');
+const $mobileMenuUser = document.querySelector('.mobile-menu__user--js');
+const $mobileMenuLogin = document.querySelector('.mobile-menu__login--js');
+const $myAccountSection = document.querySelector('.my-account-section--js');
+const $editAccountSection = document.querySelector('.edit-account-section--js');
+const $newEmail = document.getElementById('new-email');
+const $newPassword = document.getElementById('new-password1');
+const $confirmNewPassword = document.getElementById('new-password2');
 
 const api = axios.create({
   baseURL: 'https://fakestoreapi.com/products',
@@ -54,18 +54,18 @@ function renderCategories(items) {
     aItem1.classList.add('margin--right', 'main-text');
     aItem1.innerText = upperCase(item);
 
-    categories.appendChild(liItem1);
+    $categoriesDesktop.appendChild(liItem1);
     liItem1.appendChild(aItem1);
 
     const liItem2 = document.createElement('li');
-    liItem2.classList.add('margin--top0_8', 'margin--left', 'margin--right');
+    liItem2.classList.add('margin--top0_8', 'margin--left');
 
     const aItem2 = document.createElement('a');
     aItem2.setAttribute('href', 'javascript:void(0)');
     aItem2.classList.add('main-text');
     aItem2.innerText = upperCase(item);
     
-    mobileMenuCategories.appendChild(liItem2);
+    $mobileMenuCategories.appendChild(liItem2);
     liItem2.appendChild(aItem2);
   });
 }
@@ -81,61 +81,61 @@ const getCategories = async() => {
 
 function toggleNavbarNav(state) {
   if(state == 'inactive') {
-    navbarNav.classList.add('inactive');
-    navbarNav.classList.remove('active');
+    $navbarNav.classList.add('inactive');
+    $navbarNav.classList.remove('active');
   } else if(state == 'active'){
-    navbarNav.classList.remove('inactive');
-    navbarNav.classList.add('active');
+    $navbarNav.classList.remove('inactive');
+    $navbarNav.classList.add('active');
   }
 }
 
 function toggleHamburguerIcon(state) {
   if(state == 'visible') {
-    hamburguerIcon.classList.remove('invisible');
+    $hamburguerIcon.classList.remove('invisible');
   } else if(state == 'invisible'){
-    hamburguerIcon.classList.add('invisible');
+    $hamburguerIcon.classList.add('invisible');
   }
   
 }
 
 function toggleHamburguerIconSize(size) {
   if(size == 'medium') {
-    hamburguerIcon.classList.remove('icon--large');
-    hamburguerIcon.classList.add('icon--medium');
+    $hamburguerIcon.classList.remove('icon--large');
+    $hamburguerIcon.classList.add('icon--medium');
   } else if(size == 'large'){
-    hamburguerIcon.classList.add('icon--large');
-    hamburguerIcon.classList.remove('icon--medium');
+    $hamburguerIcon.classList.add('icon--large');
+    $hamburguerIcon.classList.remove('icon--medium');
   }
   
 }
 
 function toggleIcons(device) {
   if(device == 'mobile') {
-    icons.classList.add('icons--mobile');
-    icons.classList.remove('icons--desktop');
+    $icons.classList.add('icons--mobile');
+    $icons.classList.remove('icons--desktop');
   } else if(device == 'desktop'){
-    icons.classList.add('icons--desktop');
-    icons.classList.remove('icons--mobile');
+    $icons.classList.add('icons--desktop');
+    $icons.classList.remove('icons--mobile');
   }
 }
 
 function toggleLogoSize(size) {
   if(size == 'medium') {
-    logo.classList.remove('logo--large');
+    $logo.classList.remove('logo--large');
   } else if(size == 'large'){
-    logo.classList.add('logo--large');
+    $logo.classList.add('logo--large');
   }
 }
 
 function toggleCartIconSize(size) {
   if(size == 'medium') {
-    cartIcon.classList.remove('icon--large');
-    cartIcon.classList.add('icon--medium');
-    cartIconSvg.classList.remove('icon-svg--large');
+    $cartIcon.classList.remove('icon--large');
+    $cartIcon.classList.add('icon--medium');
+    $cartIconSvg.classList.remove('icon-svg--large');
   } else if(size == 'large'){
-    cartIcon.classList.remove('icon--medium');
-    cartIcon.classList.add('icon--large');
-    cartIconSvg.classList.add('icon__svg--large');
+    $cartIcon.classList.remove('icon--medium');
+    $cartIcon.classList.add('icon--large');
+    $cartIconSvg.classList.add('icon__svg--large');
   }
 }
 
@@ -148,16 +148,16 @@ function mediumAndLargeScreens() {
 
 function toggleMobileMenuSize(size) {
   if(size == 'small') {
-    mobileMenu.classList.add('mobile-menu--small');
-    mobileMenu.classList.remove('mobile-menu--medium');
+    $mobileMenu.classList.add('mobile-menu--small');
+    $mobileMenu.classList.remove('mobile-menu--medium');
   } else if(size == 'medium'){
-    mobileMenu.classList.add('mobile-menu--medium');
-    mobileMenu.classList.remove('mobile-menu--small');
+    $mobileMenu.classList.add('mobile-menu--medium');
+    $mobileMenu.classList.remove('mobile-menu--small');
   }
 }
 
 function userLoggingIn() {
-  window.location.href="./index.html?email="+email.value;
+  window.location.href="./index.html?email="+$email.value;
   goToHome();
 }
 
@@ -166,20 +166,20 @@ function userLoggedIn() {
   const urlParams = new URLSearchParams(queryString);
   const userEmail = urlParams.get("email");
   if(userEmail !== null) {
-    userNameDesktop.classList.remove('inactive');
-    mobileMenuUser.classList.remove('inactive');
-    loginLink.classList.add('inactive');
-    signupLink.classList.add('inactive');
-    mobileMenuLogin.classList.add('inactive');
-    userNameDesktop.text = userEmail;
-    userNameMobile.text = userEmail;
+    $userNameDesktop.classList.remove('inactive');
+    $mobileMenuUser.classList.remove('inactive');
+    $loginLink.classList.add('inactive');
+    $signupLink.classList.add('inactive');
+    $mobileMenuLogin.classList.add('inactive');
+    $userNameDesktop.text = userEmail;
+    $userNameMobile.innerHTML= userEmail;
     isUserLoggedIn = true;
     } else {
-      userNameDesktop.classList.add('inactive');
-      mobileMenuUser.classList.add('inactive');
-      loginLink.classList.remove('inactive');
-      signupLink.classList.remove('inactive');
-      mobileMenuLogin.classList.remove('inactive');
+      $userNameDesktop.classList.add('inactive');
+      $mobileMenuUser.classList.add('inactive');
+      $loginLink.classList.remove('inactive');
+      $signupLink.classList.remove('inactive');
+      $mobileMenuLogin.classList.remove('inactive');
       isUserLoggedIn = false;
     }
 }
@@ -190,40 +190,40 @@ function userLoggedOut() {
 }
 
 function toggleDesktopMenu() {
-  const isDesktopMenuOpen = !desktopMenu.classList.contains('close');
+  const isDesktopMenuOpen = !$desktopMenu.classList.contains('close');
   if (isDesktopMenuOpen){
-    desktopMenu.classList.add('close');
-    desktopMenu.classList.remove('desktop-menu--open');
-    desktopMenu.classList.add('desktop-menu--close');
+    $desktopMenu.classList.add('close');
+    $desktopMenu.classList.remove('desktop-menu--open');
+    $desktopMenu.classList.add('desktop-menu--close');
   } else {
-    desktopMenu.classList.remove('close');
-    desktopMenu.classList.add('desktop-menu--open');
-    desktopMenu.classList.remove('desktop-menu--close');
+    $desktopMenu.classList.remove('close');
+    $desktopMenu.classList.add('desktop-menu--open');
+    $desktopMenu.classList.remove('desktop-menu--close');
   }
 }
 
 function toggleMobileMenu() {
-  const isMobileMenuOpen = !mobileMenu.classList.contains('close');
+  const isMobileMenuOpen = !$mobileMenu.classList.contains('close');
   if (isMobileMenuOpen) {
-    hamburguerIcon.classList.remove('icon—pressed');
-    mobileMenu.classList.add('close');
-    mobileMenu.classList.remove('mobile-menu--open');
-    mobileMenu.classList.add('mobile-menu--close');
+    $hamburguerIcon.classList.remove('icon—pressed');
+    $mobileMenu.classList.add('close');
+    $mobileMenu.classList.remove('mobile-menu--open');
+    $mobileMenu.classList.add('mobile-menu--close');
   } else {
-    hamburguerIcon.classList.add('icon—pressed');
-    mobileMenu.classList.remove('close');
-    mobileMenu.classList.add('mobile-menu--open');
-    mobileMenu.classList.remove('mobile-menu--close');
+    $hamburguerIcon.classList.add('icon—pressed');
+    $mobileMenu.classList.remove('close');
+    $mobileMenu.classList.add('mobile-menu--open');
+    $mobileMenu.classList.remove('mobile-menu--close');
   }
 }
 
 function closeMobileMenu() {
-  mobileMenu.classList.remove('close');
+  $mobileMenu.classList.remove('close');
   toggleMobileMenu();
 }
 
 function closeDesktopMenu() {
-  desktopMenu.classList.remove('close');
+  $desktopMenu.classList.remove('close');
   toggleDesktopMenu();
 }
 
@@ -236,15 +236,15 @@ function openLoginSection() {
   closeMobileMenu();
   closeDesktopMenu();
   toggleHamburguerIcon('invisible');
-  cartIcon.classList.add('invisible');
-  loginSection.classList.remove('inactive');
+  $cartIcon.classList.add('invisible');
+  $loginSection.classList.remove('inactive');
 }
 
 function closeLoginSection() {
-  cartIcon.classList.remove('invisible');
-  email.value = '';
-  password.value = '';
-  loginSection.classList.add('inactive');
+  $cartIcon.classList.remove('invisible');
+  $email.value = '';
+  $password.value = '';
+  $loginSection.classList.add('inactive');
 }
 
 function openSignupSection() {
@@ -256,17 +256,17 @@ function openSignupSection() {
   closeMobileMenu();
   closeDesktopMenu();
   toggleHamburguerIcon('invisible');
-  cartIcon.classList.add('invisible');
-  signupSection.classList.remove('inactive');
+  $cartIcon.classList.add('invisible');
+  $signupSection.classList.remove('inactive');
 }
 
 function closeSignupSection() {
-  cartIcon.classList.remove('invisible');
-  newUserName.value = '';
-  newUserEmail.value = '';
-  newUserPassword.value = '';
-  confirmNewUserPassword.value = '';
-  signupSection.classList.add('inactive');
+  $cartIcon.classList.remove('invisible');
+  $newUserName.value = '';
+  $newUserEmail.value = '';
+  $newUserPassword.value = '';
+  $confirmNewUserPassword.value = '';
+  $signupSection.classList.add('inactive');
 }
 
 function openRecoveryPasswordSection() {
@@ -278,14 +278,14 @@ function openRecoveryPasswordSection() {
   closeMobileMenu();
   closeDesktopMenu();
   toggleHamburguerIcon('invisible');
-  cartIcon.classList.add('invisible');
-  recoveryPasswordSection.classList.remove('inactive');
+  $cartIcon.classList.add('invisible');
+  $recoveryPasswordSection.classList.remove('inactive');
 }
 
 function closeRecoveryPasswordSection() {
-  cartIcon.classList.remove('invisible');
-  sendEmail.value = '';
-  recoveryPasswordSection.classList.add('inactive');
+  $cartIcon.classList.remove('invisible');
+  $sendEmail.value = '';
+  $recoveryPasswordSection.classList.add('inactive');
 }
 
 function openEmailConfirmationdSection() {
@@ -297,13 +297,13 @@ function openEmailConfirmationdSection() {
   closeMobileMenu();
   closeDesktopMenu();
   toggleHamburguerIcon('invisible');
-  cartIcon.classList.add('invisible');
-  emailConfirmationSection.classList.remove('inactive');
+  $cartIcon.classList.add('invisible');
+  $emailConfirmationSection.classList.remove('inactive');
 }
 
 function closeEmailConfirmationSection() {
-  cartIcon.classList.remove('invisible');
-  emailConfirmationSection.classList.add('inactive');
+  $cartIcon.classList.remove('invisible');
+  $emailConfirmationSection.classList.add('inactive');
 }
 
 function openMyAccount() {
@@ -311,24 +311,24 @@ function openMyAccount() {
   closeDesktopMenu();
   toggleNavbarNav('inactive');
   toggleHamburguerIcon('invisible');
-  cartIcon.classList.add('invisible');
-  myAccountSection.classList.remove('inactive');
+  $cartIcon.classList.add('invisible');
+  $myAccountSection.classList.remove('inactive');
 }
 
 function closeMyAccount() {
-  cartIcon.classList.remove('invisible');
-  myAccountSection.classList.add('inactive');
+  $cartIcon.classList.remove('invisible');
+  $myAccountSection.classList.add('inactive');
 }
 
 function openEditMyAccount() {
-  editAccountSection.classList.remove('inactive');
+  $editAccountSection.classList.remove('inactive');
 }
 
 function closeEditMyAccount() {
-  editAccountSection.classList.add('inactive');
-  newEmail.value = '';
-  newPassword.value = '';
-  confirmNewPassword.value = '';
+  $editAccountSection.classList.add('inactive');
+  $newEmail.value = '';
+  $newPassword.value = '';
+  $confirmNewPassword.value = '';
 }
 
 function resizeHandler() {
